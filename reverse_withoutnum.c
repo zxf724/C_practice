37e27a247111a09c36294564ae96403d9d5d3c81
wenzong <565737064@qq.com>
2020-03-20 10:29:21 +0800

    添加倒叙字符串且去掉数字

--- reverse_withoutnum.c
+++ reverse_withoutnum.c
@@ -6,15 +6,17 @@
 
 #define LEN 100
 
+void reverse_string(char *src);
+
 void main(void) {
-	char src[LEN] = "abcdefg";
+	char src[LEN] = "0a1b2c3d4e5f6g7";
 	printf("%s\n", src);
 
 	reverse_string(src);
 	printf("%s\n", src);
 }
 
-reverse_string(char* src) {
+void reverse_string(char* src) {
   char *p = src;
   uint8_t len = 0; 
     
@@ -25,12 +27,19 @@ reverse_string(char* src) {
   printf("%d\n", len);
 
   char c;	  		// reverse
-  uint8_t i=0;
+  uint8_t i=0,j=0;
   while(i<=len/2-1) {
 	c = *(src+i);
 	*(src+i) = *(src+len-i-1);
 	*(src+len-i-1) = c;
 	i++;  	
   }
+  for(i=0,j=0;i<=len;i++) {
+  	if((*(src+i) <'0') || (*(src+i) > '9')) {
+  		*(src+j) = *(src+i);
+  		j++;
+  	}
+  }
+	*(src+j) = '\0';
 }
 
